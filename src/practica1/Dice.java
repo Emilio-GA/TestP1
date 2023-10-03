63ba11f6f7f34e53c53bbf914a0aad12698189db
egalv <egalv@LAPTOP-9DH4RT51>
2023-10-03 18:36:56 +0200

    sumando

--- src/practica1/Dice.java
+++ src/practica1/Dice.java
@@ -1,10 +1,10 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Practica1;
 import java.util.Random;
-
 
 
 /**
@@ -170,4 +170,5 @@ public class Dice {
         
         return discard;     
     }
+
 }

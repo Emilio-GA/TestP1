f9346687d75f410d7f3be9454f9cb0686ed806d8
egalv <egalv@LAPTOP-9DH4RT51>
2023-10-04 17:16:37 +0200

    Merge origin/master
    Conflicts:
    	nbproject/private/private.properties
    	src/practica1/TestP1.java

---
+++ src/practica1/Monster.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1;
+
+/**
+ *
+ * @author egalv
+ */
+public class Monster {
+    
+       
+    private int INITIAL_HEALTH = 5;
+    private String name;
+    private  float intelligence;
+    private float stength;
+    private float health;
+    private int row;
+    private int col;
+    
+    public Monster (String name, float intelligence, float strength){
+        
+        this.name = name;
+        this.intelligence = intelligence;
+        this.stength = strenght
+    }
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}

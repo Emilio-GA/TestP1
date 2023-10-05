6b8bbe2d8f395c8e5d624639a34348aaa8f14396
AlbertoRodriguezFernandez <arf170903@gmail.com>
2023-10-05 17:22:10 +0200

    practica 2

--- src/practica1/Labyrinth.java
+++ src/practica1/Labyrinth.java
@@ -2,12 +2,79 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package practica1;
+package Practica1;
+
+
 
 /**
- *
- * @author alberto
+ * @author Alberto Rodriguez Fernandez
+ * @author Emilio Guillen Alvarez
  */
+
+// CLASE QUE REPRESENTA EL LABERINTO DEL JUEGO
 public class Labyrinth {
     
+    //
+    private static char BLOCK_CHAR = 'X';
+    
+    
+    //
+    private static char EMPTY_CHAR = '-';
+    
+    
+    //
+    private static char MONSTER_CHAR = 'M';
+    
+    
+    //
+    private static char COMBAT_CHAR = 'C';
+    
+    
+    //
+    private static char EXIT_CHAR = 'E';
+    
+    
+    //
+    private static int ROW = 0;
+    
+    
+    //
+    private static int COL = 1;
+    
+    
+    //
+    private int nRows;
+    
+    
+    //
+    private int nCols;
+    
+    
+    //
+    private int exitRow;
+    
+    
+    //
+    private int exitCol;
+    
+    
+    // CONSTRUCTOR
+    Labyrinth(int row, int co, int eRow, int eCol) {
+        
+        
+    }
+    
+    
+    // METODO SPREADPLAYERS
+    void spreadPlayers(Player[] players) {
+        
+        
+    }
+    
+    
+    // METODO HAVEAWINNER
+    boolean haveAWinner() {
+        
+        
+    }      
 }

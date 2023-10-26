5755ae40172c9086b7217da4cdaca78902f7696f
AlbertoRodriguezFernandez <arf170903@gmail.com>
2023-10-26 17:24:39 +0200

    ultimos cambios

--- src/practica1/Game.java
+++ src/practica1/Game.java
@@ -63,6 +63,9 @@ public class Game {
         // Laberinto
         labyrinth = new Labyrinth(5, 6, 3, 3);
         
+        this.configureLabyrinth();
+        // spreadPlayers(players);
+        
     }
     
     
